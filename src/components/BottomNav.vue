26391a7e96b2db6308f44c9774794c88468f5d30
HAMID <mer30hamid@gmail.com>
2022-02-25 17:00:29 +0330

    initial look and feel and design enable dark mode

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -7,20 +7,16 @@
     v-model="activeItem"
     color="primary"
   >
-    <v-btn value="top">
-      <span>Top Stories</span>
-      <v-icon>thumb_up</v-icon>
-    </v-btn>
-
     <v-btn value="code">
-      <span>Code Examples</span>
-      <v-icon>code</v-icon>
+      <span>Home</span>
+      <v-icon>home</v-icon>
     </v-btn>
 
-    <v-btn value="favorites">
-      <span>Favorites</span>
-      <v-icon>favorite</v-icon>
+    <v-btn value="top">
+      <span>Share</span>
+      <v-icon>share</v-icon>
     </v-btn>
+
   </v-bottom-navigation>
 
 </template>
--- src/components/TopToolbar.vue
+++ src/components/TopToolbar.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-
-    <v-navigation-drawer
-      app
-      fixed
-      v-model="showMenu"
-
-    >
+    <v-navigation-drawer app fixed v-model="showMenu">
       <v-list dense>
         <v-list-item @click="doNothing">
           <v-list-item-action>
@@ -27,31 +21,37 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      fixed
-      dark
-      color="primary"
-    >
+    <v-app-bar app fixed dark color="primary">
       <v-app-bar-nav-icon @click.stop="toggleMenu"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Vue News</v-toolbar-title>
+      <v-toolbar-title>Metro Yaab!</v-toolbar-title>
 
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="darkMode">
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-information</v-icon>
+      </v-btn>
     </v-app-bar>
-
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  @Component
-  export default class TopToolbar extends Vue {
-    showMenu = false;
-    toggleMenu(): void {
-      this.showMenu = !this.showMenu;
-    }
-    doNothing(): void {
-     // do nothing. 
-    }
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class TopToolbar extends Vue {
+  showMenu = false;
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
   }
+  doNothing(): void {
+    // do nothing.
+  }
+  darkMode() {
+    this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    localStorage.setItem('isDark', this.$vuetify.theme.dark + "")
+  }
+}
 </script>
